609aa94890add6ba33baf60e5ade20f1615538ca
zhaoliyuan <3316333839@qq.com>
2021-01-11 09:41:04 +0800

    修改跨域

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -5,12 +5,7 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-<<<<<<< HEAD
   window.SITE_CONFIG['baseUrl'] = 'http://192.144.229.232:51110/manage';
-=======
-  window.SITE_CONFIG['baseUrl'] = 'http://192.144.229.232:51120/manage/';
->>>>>>> 7802886d2a3d4c6081b6963fbd18a989897c4af2
-
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
   window.SITE_CONFIG['version'] = '';   // 版本号(年月日时分)
